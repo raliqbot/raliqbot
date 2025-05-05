7ee73a974d17e50dec652d98e95e5812f7118c71
Include caleb.fun <webmaster@apexflix.com>
2025-05-05 13:01:22 +0100

    wip

--- web/media/src/app/api/screenshot/route.ts
+++ web/media/src/app/api/screenshot/route.ts
@@ -1,46 +1,56 @@
 import puppeteer from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 import type { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
   const url = searchParams.get("url");
+  const browser =
+    process.env.NODE_ENV === "development"
+      ? puppeteer.launch({
+          headless: true,
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        })
+      : puppeteerCore.launch({
+          args: chromium.args,
+          headless: chromium.headless,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath(),
+        });
 
-  if (url)
-    return puppeteer
-      .launch({
-        headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      })
-      .then(async (browser) => {
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: "networkidle2", timeout: 30000 });
-        await page.waitForSelector("#media");
-        const element = await page.$("#media");
-        if (element) {
-          const buffer = await element.screenshot({
-            type: "png",
-          });
+  if (url) return;
+  browser
+    .then(async (browser) => {
+      const page = await browser.newPage();
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 30000 });
+      await page.waitForSelector("#media");
+      const element = await page.$("#media");
+      if (element) {
+        const buffer = await element.screenshot({
+          type: "png",
+        });
 
-          await browser.close();
+        await browser.close();
 
-          return new Response(buffer, {
-            headers: {
-              "Content-Type": "image/png",
-              "Cache-Control": "no-store",
-            },
-          });
-        } else {
-          page.close();
-          browser.close();
-          return new Response("This page is not a valid media page", {
-            status: 404,
-          });
-        }
-      })
-      .catch((error) => {
-        console.error("[Screenshot error]", error);
-        return new Response("Failed to take screenshot", { status: 500 });
-      });
+        return new Response(buffer, {
+          headers: {
+            "Content-Type": "image/png",
+            "Cache-Control": "no-store",
+          },
+        });
+      } else {
+        page.close();
+        browser.close();
+        return new Response("This page is not a valid media page", {
+          status: 404,
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("[Screenshot error]", error);
+      return new Response("Failed to take screenshot", { status: 500 });
+    });
 
   return new Response('Missing "url" query parameter', { status: 400 });
 };
