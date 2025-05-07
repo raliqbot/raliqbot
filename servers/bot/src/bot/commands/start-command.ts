f6c92ecefc635f629d6aa64b21582df896df1a94
Include caleb.fun <webmaster@apexflix.com>
2025-05-07 17:59:06 +0100

    feat: wip

--- servers/bot/src/bot/commands/start-command.ts
+++ servers/bot/src/bot/commands/start-command.ts
@@ -12,6 +12,7 @@ export const startCommand = (telegraf: Telegraf) => {
     const { wallet } = context;
 
     if (context.message.text) {
+      console.log(context.message.text);
       if (/open/.test(context.message.text)) return onOpenPosition(context);
       if (/createPosition/.test(context.message.text))
         return onCreatePosition(context);
