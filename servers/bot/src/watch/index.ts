e79a86bbdb815be7d6895994053bc5ab2af95d08
Include caleb.fun <webmaster@apexflix.com>
2025-05-10 22:09:57 +0100

    feat: bug fixture

--- servers/bot/src/watch/index.ts
+++ servers/bot/src/watch/index.ts
@@ -1,11 +1,11 @@
-import { web3 } from "@coral-xyz/anchor";
-import { bitquery, bot, db } from "../instances";
-import { positionChecks } from "./position-check";
+// import { web3 } from "@coral-xyz/anchor";
+// import { bitquery, bot, db } from "../instances";
+// import { positionChecks } from "./position-check";
 
 
-await positionChecks(
-  db,
-  bot,
-  bitquery,
-  new web3.Connection(web3.clusterApiUrl("mainnet-beta"))
-);
+// await positionChecks(
+//   db,
+//   bot,
+//   bitquery,
+//   new web3.Connection(web3.clusterApiUrl("mainnet-beta"))
+// );
