e2b3e332ace4d3e02df105494a5a5431683bf20a
Include caleb.fun <webmaster@apexflix.com>
2025-05-10 22:06:14 +0100

    feat: bug fixture

--- packages/lib/src/bitquery/price.api.ts
+++ packages/lib/src/bitquery/price.api.ts
@@ -1,14 +1,7 @@
-import path from "path";
-import { readFileSync } from "fs";
-import { fileURLToPath } from 'url';
 import { ApiImpl } from "@raliqbot/shared";
 
 import type { Price } from "./models/price.model";
 import type { Response } from "./models/response.model";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export class PriceApi extends ApiImpl {
   protected path: string = "eap";
@@ -18,11 +11,10 @@ export class PriceApi extends ApiImpl {
     quoteToken: string;
     poolId: string;
   }) {
+    const query = `query PairPrice($baseToken: String!, $quoteToken: String!, $poolId: String) {\n  Solana(dataset: realtime) {\n    DEXTradeByTokens(\n      where: {\n        Transaction: { Result: { Success: true } },\n        Trade: {\n          Currency: { MintAddress: { is: $baseToken } },\n          Side: { Currency: { MintAddress: { is: $quoteToken } } },\n          Market: { MarketAddress: { is: $poolId } }\n        }\n      },\n      limitBy: { count: 1, by: Trade_Dex_ProtocolName },\n      orderBy: { descending: Trade_PriceInUSD }\n    ) {\n      Trade {\n        Price\n        PriceInUSD\n        PriceAsymmetry\n      }\n    }\n  }\n}`;
+
     return this.xior.post<Response<Price>>(this.path, {
-      query: readFileSync(
-        path.join(__dirname, "queries/fetch_price.graphql"),
-        "utf-8"
-      ),
+      query,
       variables,
     });
   }
