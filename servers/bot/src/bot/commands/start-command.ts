65840a0d2d2e553cebe079655d3a988f85fb4678
Include caleb.fun <webmaster@apexflix.com>
2025-06-08 13:06:21 +0100

    feat: bug fixes

--- servers/bot/src/bot/commands/start-command.ts
+++ servers/bot/src/bot/commands/start-command.ts
@@ -11,7 +11,7 @@ export const startCommand = (telegraf: Telegraf) => {
   telegraf.start(
     atomic(async (context) => {
       const { wallet } = context;
-      await context.scene.leave();
+      if (context.scene.current) await context.scene.leave();
 
       if (context.message.text) {
         if (/open/.test(context.message.text)) return onOpenPosition(context);
@@ -24,48 +24,6 @@ export const startCommand = (telegraf: Telegraf) => {
       if (context.chat.type === "private") {
         const solBalance =
           (await connection.getBalance(wallet.publicKey)) / Math.pow(10, 9);
-
-        // return context.telegram
-        //   .approveChatJoinRequest(getEnv("CHANNNEL_ID"), context.from.id)
-        //   .catch((error) => {
-        //     if (error instanceof TelegramError) {
-        //       if (error.description.includes("USER_ALREADY_PARTICIPANT")) {
-        //         return context.replyWithAnimation(
-        //           Input.fromLocalFile("assets/welcome.gif"),
-        //           {
-        //             caption: readFileSync(
-        //               "locale/en/start/welcome-message.md",
-        //               "utf-8"
-        //             )
-        //               .replace(
-        //                 "%address%",
-        //                 cleanText(wallet.publicKey.toBase58())
-        //               )
-        //               .replace("%balance%", cleanText(String(solBalance))),
-        //             parse_mode: "MarkdownV2",
-        //             reply_markup: Markup.inlineKeyboard([
-        //               [
-        //                 Markup.button.callback(
-        //                   "➕ Open Position",
-        //                   "open_position"
-        //                 ),
-        //               ],
-        //               [
-        //                 Markup.button.callback("💼 Porfolio", "portfolio"),
-        //                 Markup.button.callback("💳 Wallet", "wallet"),
-        //               ],
-        //               [
-        //                 Markup.button.callback("⚙️ Settings", "settings"),
-        //                 Markup.button.callback("🆘 Help", "help"),
-        //               ],
-        //             ]).reply_markup,
-        //           }
-        //         );
-        //       }
-
-        //       console.error(error);
-        //     }
-        //   });
 
         return context.replyWithAnimation(
           Input.fromLocalFile("assets/welcome.gif"),
