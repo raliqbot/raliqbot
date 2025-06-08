5f3069b027f34b201fdc208b8887d6dee9cbdf11
Include caleb.fun <webmaster@apexflix.com>
2025-06-08 13:28:44 +0100

    feat: bug fix

--- servers/bot/src/bot/index.ts
+++ servers/bot/src/bot/index.ts
@@ -1,14 +1,33 @@
-import { Scenes, session, type Telegraf } from "telegraf";
+import { parse } from "pg-connection-string";
+import { Postgres } from "@telegraf/session/pg";
+import { Scenes, session, type SessionStore, type Telegraf } from "telegraf";
 
+import { getEnv } from "../core";
 import { scenes } from "./scenes";
 import registerActions from "./actions";
+import { SessionData } from "../global";
 import registerCommands from "./commands";
 import { authenticateUser } from "./middlewares/authenticate-user";
 
 export default function registerBot(bot: Telegraf) {
+  const config = parse(getEnv("DATABASE_URL"));
   const stage = new Scenes.Stage<any>(scenes);
+  const store: SessionStore<SessionData> = Postgres({
+    user: config.user!,
+    host: config.host!,
+    password: config.password!,
+    database: config.database!,
+    config: {
+      ssl: config.ssl as boolean,
+    },
+  });
+
   scenes.map((scene) => scene.use(authenticateUser));
-  bot.use(session());
+  bot.use(
+    session({
+      store,
+    })
+  );
   bot.use(stage.middleware());
   bot.use(authenticateUser);
 
--- servers/bot/src/bot/utils/index.ts
+++ servers/bot/src/bot/utils/index.ts
@@ -1,4 +1,4 @@
-import { Context } from "telegraf";
+import type { Context } from "telegraf";
 
 import { cleanText } from "./format";
 
